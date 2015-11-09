77fd6b2bc2e460150c9cfe6a5dca946441d2787c
Ricardo Vazquez <ricardo@mozillafoundation.org>
2015-11-09 14:54:38 +0000

    Creating Contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,20 @@
+/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
+
+import React, { Component } from 'react';
+import styles from './Contact.css';
+import withStyles from '../../decorators/withStyles';
+
+@withStyles(styles)
+class Contact extends Component {
+
+  render() {
+    return (
+      <div className="Contact">
+        <p>hello</p>
+      </div>
+    );
+  }
+
+}
+
+export default Contact;
