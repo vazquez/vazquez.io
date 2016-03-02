c0eeb0960a2acef4fc1f5e43555a9ffcbad42450
Ricardo Vazquez <ricardo@mozillafoundation.org>
2016-03-02 10:37:44 -0500

    Adding footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,30 +10,31 @@ import Link from '../Link';
 @withStyles(styles)
 class Footer extends Component {
 
-  static propTypes = {
-    viewport: PropTypes.shape({
-      width: PropTypes.number.isRequired,
-      height: PropTypes.number.isRequired,
-    }).isRequired,
-  };
+  // static propTypes = {
+  //   viewport: PropTypes.shape({
+  //     width: PropTypes.number.isRequired,
+  //     height: PropTypes.number.isRequired,
+  //   }).isRequired,
+  // };
 
   render() {
-    // This is just an example how one can render CSS
-    const { width, height } = this.props.viewport;
-    this.renderCss(`.Footer-viewport:after {content:' ${width}x${height}';}`);
+    // // This is just an example how one can render CSS
+    // const { width, height } = this.props.viewport;
+    // this.renderCss(`.Footer-viewport:after {content:' ${width}x${height}';}`);
 
     return (
       <div className="Footer">
         <div className="Footer-container">
-          <span className="Footer-text">© Your Company</span>
+          <span className="Footer-text--muted" target="_blank">Made with love on a train </span>
           <span className="Footer-spacer">·</span>
-          <a className="Footer-link" href="/" onClick={Link.handleClick}>Home</a>
+          <a className="Footer-link" href="http://medium.com/@iamrvazquez" target="_blank">Medium</a>
           <span className="Footer-spacer">·</span>
-          <a className="Footer-link" href="/privacy" onClick={Link.handleClick}>Privacy</a>
+          <a className="Footer-link" href="http://twitter.com/iamrvazquez" target="_blank">Twitter</a>
           <span className="Footer-spacer">·</span>
-          <a className="Footer-link" href="/not-found" onClick={Link.handleClick}>Not Found</a>
-          <span className="Footer-spacer"> | </span>
-          <span ref="viewport" className="Footer-viewport Footer-text Footer-text--muted">Viewport:</span>
+          <a className="Footer-link" href="http://github.com/vazquez" target="_blank">GitHub</a>
+          {/*<a className="Footer-link" href="/not-found" onClick={Link.handleClick}>Not Found</a>*/}
+          {/*<span className="Footer-spacer"> | </span>
+          <span ref="viewport" className="Footer-viewport Footer-text Footer-text--muted">Viewport:</span>*/}
         </div>
       </div>
     );
